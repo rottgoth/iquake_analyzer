13522f2ad84d331578c30e024c1f6e08530fc69c
Rob Cataneo <robcataneo@gmail.com>
2018-05-11 07:38:15 -0700

    Refactored iquake analyazer to reuse functionality

--- app/models/iquake_analyzer.rb
+++ app/models/iquake_analyzer.rb
@@ -7,19 +7,26 @@ class IquakeAnalyzer
   end
 
   def list_california_earthquakes
-    # alternative approach
-    # data.group_by { |earthquake| earthquake[:city] }['CA']
-    data.select { |earthquake| earthquake[:city] == 'CA' }
-        .sort { |a, b| b[:mag] <=> a[:mag] }
+    sort_earthquakes data_by_city['CA']
   end
 
   def list_top_us_cities_earthquakes
-    data.group_by { |earthquake| earthquake[:city] }
-        .sort { |a, b| b.last.count <=> a.last.count }
-        .take(3)
-        .reduce([]) do |acc, current_city|
-            city, earthquakes = current_city
-            acc.concat earthquakes.sort { |a, b| b[:mag] <=> a[:mag] }
-        end
+    data_by_city
+      .sort { |a, b| b.last.count <=> a.last.count } # sort cities by highest earthquakes count
+      .take(3) # take the first 3 cities (this could be parameterized)
+      .reduce([]) do |acc, current_city|
+        # sort earthquakes by mag within each city anc join them in the same loop
+        city, earthquakes = current_city
+        acc.concat sort_earthquakes(earthquakes)
+      end
+  end
+
+  private
+  def data_by_city
+    @data_by_city ||= data.group_by { |earthquake| earthquake[:city] }
+  end
+
+  def sort_earthquakes(list = [])
+    list.sort { |a, b| b[:mag] <=> a[:mag] }
   end
 end
