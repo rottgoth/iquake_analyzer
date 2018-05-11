12fd97c3f4f3dfb8bbd6b6c53849c1f54de1b855
Rob Cataneo <robcataneo@gmail.com>
2018-05-10 18:15:26 -0700

    Initial implementation of data parser

--- app/models/usgs_client.rb
+++ app/models/usgs_client.rb
@@ -13,4 +13,39 @@ class UsgsClient
     # parse response
     self.json = JSON.parse(response)
   end
+
+  class DataParser
+    # from https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+    attr_accessor :data
+    # information we need: id, mag, place, time, tz
+    FEATURE_ATTRIBUTES = ['mag', 'place', 'time', 'tz']
+
+    def initialize(data={})
+      @data = data
+    end
+
+    def process_data
+      features = data['features'] || []
+      features.each do |feature|
+        process_feature(feature)
+      end
+    end
+
+    def process_feature(feature = {})
+      id = feature['id']
+      attributes = feature['properties'].slice(*FEATURE_ATTRIBUTES)
+      state = get_state_from_place(attributes['place'])
+      return {
+        id: id,
+        place: attributes['place'],
+        state: state,
+        time: attributes['time'],
+        tz: attributes['tz'],
+      }
+    end
+
+    def get_state_from_place(place)
+      place.match(/,\s(.*)$/) { |m| m[1] }
+    end
+  end
 end
--- spec/models/usgs_client_spec.rb
+++ spec/models/usgs_client_spec.rb
@@ -2,6 +2,17 @@ require 'rails_helper'
 
 RSpec.describe UsgsClient, type: :model do
   let(:client) { UsgsClient.new }
+  let(:response) do
+      path = Rails.root.join('spec/support/usgs_fake_response.json')
+      file = File.open(path, "r")
+      response = file.read
+      file.close
+
+      response
+    end
+
+  let(:json_response) { JSON.parse(response) }
+
   subject { client }
 
   describe '#initialize' do
@@ -11,16 +22,6 @@ RSpec.describe UsgsClient, type: :model do
   end
 
   describe '#fetch_data' do
-    let(:response) do
-      path = Rails.root.join('spec/support/usgs_fake_response.json')
-      file = File.open(path, "r")
-      response = file.read
-      file.close
-
-      response
-    end
-
-    let(:json_response) { JSON.parse(response) }
     let(:uri) { URI(client.api_uri) }
     subject { client.fetch_data }
 
@@ -37,5 +38,70 @@ RSpec.describe UsgsClient, type: :model do
       subject
       expect(client.json).to eql(json_response)
     end
+  end
+
+  describe UsgsClient::DataParser do
+    let(:parser) { UsgsClient::DataParser.new(json_response) }
+    let(:features_count) { json_response['features'].count }
+    let(:feature) { json_response['features'][0] }
+
+    describe '#process_data' do
+      before(:each) do
+        allow(parser).to receive(:process_feature).and_return true
+      end
+
+      subject { parser.process_data }
+
+      it 'processes features from dataset' do
+        expect(parser).to receive(:process_feature).exactly(features_count).times
+        subject
+      end
+    end
+
+    describe '#processes_feature' do
+      subject { parser.process_feature(feature) }
+      let(:properties) { feature['properties'] }
+
+      it 'should return id' do
+        expect(subject).to include(id: feature['id'])
+      end
+
+      it 'should return place' do
+        expect(subject).to include(place: properties['place'])
+      end
+
+      it 'should return time' do
+        expect(subject).to include(time: properties['time'])
+      end
+
+      it 'should return tz' do
+        expect(subject).to include(tz: properties['tz'])
+      end
+
+      it 'should return state' do
+        place = properties['place']
+        expect(parser).to receive(:get_state_from_place).with(place).and_return 'CA'
+        expect(subject).to include(state: 'CA')
+      end
+    end
+
+    describe '#get_state_from_place' do
+      let(:place) { feature['properties']['place'] } # "15km SW of Leilani Estates, Hawaii"
+      subject { parser.get_state_from_place(place) }
+
+      it { is_expected.to eql('Hawaii') }
+
+      it 'should get the state' do
+        state = parser.get_state_from_place("15km SW of Leilani Estates, Hawaii")
+        expect(state).to eql('Hawaii')
+
+        state = parser.get_state_from_place("9km NE of Aguanga, CA")
+        expect(state).to eql('CA')
+
+        state = parser.get_state_from_place("90km NW of Talkeetna, Alaska")
+        expect(state).to eql('Alaska')
+      end
+    end
+
   end
 end
