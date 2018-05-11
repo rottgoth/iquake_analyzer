209d0c7505e0ac32b82b08b40ddbf0978a8b52bc
Rob Cataneo <robcataneo@gmail.com>
2018-05-11 07:28:33 -0700

    Initial implementation of #list_top_us_cities_earthquakes

--- app/models/iquake_analyzer.rb
+++ app/models/iquake_analyzer.rb
@@ -12,4 +12,14 @@ class IquakeAnalyzer
     data.select { |earthquake| earthquake[:city] == 'CA' }
         .sort { |a, b| b[:mag] <=> a[:mag] }
   end
+
+  def list_top_us_cities_earthquakes
+    data.group_by { |earthquake| earthquake[:city] }
+        .sort { |a, b| b.last.count <=> a.last.count }
+        .take(3)
+        .reduce([]) do |acc, current_city|
+            city, earthquakes = current_city
+            acc.concat earthquakes.sort { |a, b| b[:mag] <=> a[:mag] }
+        end
+  end
 end
--- spec/models/iquake_analyzer_spec.rb
+++ spec/models/iquake_analyzer_spec.rb
@@ -4,6 +4,9 @@ RSpec.describe IquakeAnalyzer, type: :model do
 
   let(:analyzer) { IquakeAnalyzer.new }
   let(:data) { [] }
+  let(:sorting_mags) do
+    -> (records) { records.map { |record| record[:mag] }.sort { |a, b| b <=> a } }
+  end
 
   before :each do
     allow_any_instance_of(UsgsClient).to receive(:fetch_data).and_return data
@@ -48,10 +51,7 @@ RSpec.describe IquakeAnalyzer, type: :model do
       ].shuffle # to ensure random order of records
     end
 
-    let(:sorted_mags) do
-      ca_records.map { |record| record[:mag] }
-                .sort { |a,b| b <=> a } # decreasing order
-    end
+    let(:sorted_mags) { sorting_mags.(ca_records) }
 
     subject { analyzer.list_california_earthquakes }
 
@@ -67,5 +67,57 @@ RSpec.describe IquakeAnalyzer, type: :model do
       actual_mags = subject.map { |record| record[:mag] }
       expect(actual_mags).to eql(sorted_mags)
     end
+  end
+
+  describe '#list_top_us_cities_earthquakes' do
+    let(:generate_records) do
+      -> (count, city) do
+        (1..count).map { { city: city, mag: rand(50) / 10.0 } }
+      end
+    end
+
+    let(:ca_records) { generate_records.(6, 'CA') }
+    let(:hawaii_records) { generate_records.(10, 'Hawaii') }
+    let(:alaska_records) { generate_records.(4, 'Alaska') }
+    let(:texas_records) { generate_records.(2, 'Texas') }
+
+    let(:data) do
+      [
+        *ca_records,
+        *hawaii_records,
+        *alaska_records,
+        *texas_records,
+      ].shuffle # to ensure random order of records
+    end
+
+    subject { analyzer.list_top_us_cities_earthquakes }
+
+    it 'should only include top 3 cities' do
+      top_cities_count = ca_records.count + hawaii_records.count + alaska_records.count
+      expect(subject.count).to eql(top_cities_count)
+    end
+
+    it 'should not include other cities' do
+      expect(subject).to_not include(*texas_records)
+    end
+
+    it 'should sort them by city highest earthquake count first' do
+      expect(subject[0...10]).to include(*hawaii_records)
+      expect(subject[10...16]).to include(*ca_records)
+      expect(subject[16...20]).to include(*alaska_records)
+    end
+
+    it 'should sort them by decreasing mag within each city' do
+      sorted_hawaii_mags = sorting_mags.(hawaii_records)
+      sorted_ca_mags = sorting_mags.(ca_records)
+      sorted_alaska_mags = sorting_mags.(alaska_records)
+
+      get_mags = -> (records) { records.map { |record| record[:mag] } }
+      expect(get_mags.(subject[0...10])).to eql(sorted_hawaii_mags)
+      expect(get_mags.(subject[10...16])).to eql(sorted_ca_mags)
+      expect(get_mags.(subject[16...20])).to eql(sorted_alaska_mags)
+    end
+
+    # it 'should only include us cities'
   end
 end
