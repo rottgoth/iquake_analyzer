ef2d0561a84f6b707be8c15b9c4ca382bbe706ae
Rob Cataneo <robcataneo@gmail.com>
2018-05-12 17:51:15 -0700

    Added new earthquake resources to routes with new routes and root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :earthquakes, only: [:index] do
+    get :california_earthquakes, on: :collection, as: :california
+    get :top_us_cities_earthquakes, on: :collection, as: :top_us_cities
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'earthquakes#index'
 end
